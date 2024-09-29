05ed44717271a97f49818951fb844fd490918e86
kmh0831 <kmhyuk1018@gmail.com>
2024-09-29 14:11:46 +0900

    3

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -1,5 +1,5 @@
-# modules/bastion/outputs.tf
-
+# modules/bastion/outputs.tf (수정된 부분)
 output "bastion_network_interface_id" {
-  value = aws_instance.bastion.network_interface_id
+  description = "Bastion 호스트의 네트워크 인터페이스 ID"
+  value       = aws_instance.bastion.network_interface_ids[0]  # 첫 번째 네트워크 인터페이스 ID 참조
 }
