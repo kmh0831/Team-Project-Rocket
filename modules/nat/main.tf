87afdede2b6e11c08fe1384c1951ffa89485ef1b
kmh0831 <kmhyuk1018@gmail.com>
2024-09-29 22:02:33 +0900

    41

--- modules/nat/main.tf
+++ modules/nat/main.tf
@@ -1,11 +1,12 @@
+# NAT 인스턴스 생성
 resource "aws_instance" "nat" {
   count                  = length(var.nat_subnet_ids)
   ami                    = var.nat_ami
   instance_type          = var.nat_instance_type
   subnet_id              = element(var.nat_subnet_ids, count.index)
   private_ip             = element(var.nat_instance_private_ips, count.index)
-  vpc_security_group_ids = [var.security_group_id]  # 보안 그룹을 외부에서 참조
-  source_dest_check      = false
+  vpc_security_group_ids = [var.security_group_id]
+  source_dest_check      = false  # NAT 인스턴스에 대해 필요함
   key_name               = var.key_name
 
   tags = {
@@ -13,9 +14,9 @@ resource "aws_instance" "nat" {
   }
 }
 
+# NAT 인스턴스용 Elastic IP (EIP) 생성
 resource "aws_eip" "nat_eip" {
-  count = length(var.nat_subnet_ids)
-
+  count  = length(var.nat_subnet_ids)
   domain = "vpc"
 
   tags = {
@@ -23,8 +24,11 @@ resource "aws_eip" "nat_eip" {
   }
 }
 
+# EIP와 NAT 인스턴스 연결
 resource "aws_eip_association" "nat_eip_assoc" {
   count         = length(var.nat_subnet_ids)
+  
+  # NAT 인스턴스와 EIP를 정확히 매핑
   instance_id   = aws_instance.nat[count.index].id
   allocation_id = aws_eip.nat_eip[count.index].id
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "this" {
   }
 }
 
-# Public Subnets 생성
+# Public Subnets 생성 (DB VPC는 제외)
 resource "aws_subnet" "public" {
   for_each = { for i, cidr in var.vpc_config["EKS-vpc"].public_subnets : i => cidr }
 
@@ -24,7 +24,7 @@ resource "aws_subnet" "public" {
   }
 }
 
-# Private Subnets 생성
+# Private Subnets 생성 (EKS VPC)
 resource "aws_subnet" "private" {
   for_each = { for i, cidr in var.vpc_config["EKS-vpc"].private_subnets : i => cidr }
 
@@ -37,7 +37,7 @@ resource "aws_subnet" "private" {
   }
 }
 
-# DB VPC Subnets 생성 (Public 없음)
+# DB VPC Private Subnets 생성 (퍼블릭 서브넷 없음)
 resource "aws_subnet" "db_private" {
   for_each = { for i, cidr in var.vpc_config["DB-vpc"].private_subnets : i => cidr }
 
@@ -50,7 +50,7 @@ resource "aws_subnet" "db_private" {
   }
 }
 
-# Internet Gateway 생성
+# Internet Gateway 생성 (DB 퍼블릭 서브넷 제외)
 resource "aws_internet_gateway" "this" {
   for_each = aws_vpc.this
 
@@ -83,4 +83,35 @@ resource "aws_route_table_association" "public" {
 
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public["EKS-vpc"].id  # EKS VPC에 대해 Public Route Table과 연결
+}
+
+# Private Route Table 생성 (각 프라이빗 서브넷이 NAT 인스턴스와 연결)
+resource "aws_route_table" "private" {
+  for_each = { for i, cidr in var.vpc_config["EKS-vpc"].private_subnets : i => cidr }
+
+  vpc_id = aws_vpc.this["EKS-vpc"].id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_instance.nat_instance[each.key].id  # 각 NAT 인스턴스와 연결
+  }
+
+  tags = {
+    Name = "EKS-vpc-Private-RT-${each.key + 1}"
+  }
+}
+
+# Private Subnet Route Table Association
+resource "aws_route_table_association" "private" {
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private[each.key].id
+}
+
+# Bastion 호스트 라우팅 설정 (EKS 프라이빗 서브넷 3)
+resource "aws_route" "bastion_route" {
+  route_table_id         = aws_route_table.private[2].id  # EKS Private Subnet 3에 대해 설정
+  destination_cidr_block = "0.0.0.0/0"
+  instance_id            = aws_instance.bastion_host.id
 }
