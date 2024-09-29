105e05e64f6526cdea6198e815a9543b2f781f8f
kmh0831 <kmhyuk1018@gmail.com>
2024-09-29 15:45:57 +0900

    12

--- modules/nat/main.tf
+++ modules/nat/main.tf
@@ -24,32 +24,32 @@ resource "aws_security_group" "nat_sg" {
 }
 
 resource "aws_instance" "nat" {
-  for_each              = { for idx, subnet_id in var.nat_subnet_ids : idx => subnet_id }
-  ami                   = var.nat_ami
-  instance_type         = var.nat_instance_type
-  subnet_id             = each.value
-  private_ip            = var.nat_instance_private_ips[each.key]
+  count                  = length(var.nat_subnet_ids)
+  ami                    = var.nat_ami
+  instance_type          = var.nat_instance_type
+  subnet_id              = element(var.nat_subnet_ids, count.index)
+  private_ip             = element(var.nat_instance_private_ips, count.index)
   vpc_security_group_ids = [aws_security_group.nat_sg.id]
-  source_dest_check     = false
-  key_name              = var.key_name
+  source_dest_check      = false
+  key_name               = var.key_name
 
   tags = {
-    Name = "NAT-${each.key + 1}"
+    Name = "NAT-${count.index + 1}"
   }
 }
 
 resource "aws_eip" "nat_eip" {
-  for_each = aws_instance.nat
+  count = length(var.nat_subnet_ids)
 
   domain = "vpc"
 
   tags = {
-    Name = "Nat-EIP-${each.key + 1}"
+    Name = "Nat-EIP-${count.index + 1}"
   }
 }
 
 resource "aws_eip_association" "nat_eip_assoc" {
-  for_each     = aws_instance.nat
-  instance_id  = each.value.id
-  allocation_id = aws_eip.nat_eip[each.key].id
+  count         = length(var.nat_subnet_ids)
+  instance_id   = aws_instance.nat[count.index].id
+  allocation_id = aws_eip.nat_eip[count.index].id
 }
