8a8e078c1716956abcec97621d96587da0c7bdcb
kmh0831 <kmhyuk1018@gmail.com>
2024-09-29 19:09:54 +0900

    24

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,26 +11,26 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public" {
-  for_each = { for k, v in var.vpc_config : k => v.public_subnets }
+  for_each = { for az, cidr in zipmap(var.vpc_config[each.key].availability_zones, var.vpc_config[each.key].public_subnets) : az => cidr }
 
   vpc_id            = aws_vpc.this[each.key].id
-  cidr_block        = each.value[count.index]  # 각 가용 영역에 맞는 서브넷을 사용
-  availability_zone = var.vpc_config[each.key].availability_zones[count.index]  # 가용 영역을 분산
+  cidr_block        = each.value  # 각 가용 영역에 맞는 서브넷을 사용
+  availability_zone = each.key    # 각 가용 영역을 분산
 
   tags = {
-    Name = "${each.key}-Public-Subnet-${count.index + 1}"
+    Name = "${each.key}-Public-Subnet-${each.key}"
   }
 }
 
 resource "aws_subnet" "private" {
-  for_each = { for k, v in var.vpc_config : k => v.private_subnets }
+  for_each = { for az, cidr in zipmap(var.vpc_config[each.key].availability_zones, var.vpc_config[each.key].private_subnets) : az => cidr }
 
   vpc_id            = aws_vpc.this[each.key].id
-  cidr_block        = each.value[count.index]  # 각 가용 영역에 맞는 서브넷을 사용
-  availability_zone = var.vpc_config[each.key].availability_zones[count.index]  # 가용 영역을 분산
+  cidr_block        = each.value  # 각 가용 영역에 맞는 서브넷을 사용
+  availability_zone = each.key    # 각 가용 영역을 분산
 
   tags = {
-    Name = "${each.key}-Private-Subnet-${count.index + 1}"
+    Name = "${each.key}-Private-Subnet-${each.key}"
   }
 }
 
