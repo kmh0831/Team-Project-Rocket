12302f374fb3a8d3db976df0ea8d14dba2a5bc7a
kmh0831 <kmhyuk1018@gmail.com>
2024-09-29 18:37:02 +0900

    21

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -13,17 +13,17 @@ output "db_vpc_id" {
 # EKS 퍼블릭 서브넷 ID들 출력
 output "eks_public_subnet_ids" {
   description = "EKS Public Subnet IDs"
-  value       = aws_subnet.public[*].id  # 올바르게 public 서브넷을 참조
+  value       = [for subnet in values(aws_subnet.public) : subnet.id]  # 올바르게 public 서브넷을 참조
 }
 
 # EKS 프라이빗 서브넷 ID들 출력
 output "eks_private_subnet_ids" {
   description = "EKS Private Subnet IDs"
-  value       = aws_subnet.private[*].id  # 올바르게 private 서브넷을 참조
+  value       = [for subnet in values(aws_subnet.private) : subnet.id]  # 올바르게 private 서브넷을 참조
 }
 
 # DB 프라이빗 서브넷 ID들 출력
 output "db_private_subnet_ids" {
   description = "DB Private Subnet IDs"
-  value       = aws_subnet.private[*].id  # 올바르게 private 서브넷을 참조
+  value       = [for subnet in values(aws_subnet.private) : subnet.id]  # 올바르게 private 서브넷을 참조
 }
