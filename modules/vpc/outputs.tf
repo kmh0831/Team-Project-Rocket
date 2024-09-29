59972c5cbb9e4229edc2f6e394ae5523cc1d19fb
kmh0831 <kmhyuk1018@gmail.com>
2024-09-30 02:23:38 +0900

    55

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -29,11 +29,16 @@ output "db_private_subnet_ids" {
 }
 
 output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = aws_subnet.private.*.id
+  description = "List of private subnet IDs for the EKS VPC"
+  value       = [for s in aws_subnet.private : s.id]
 }
 
 output "public_subnet_ids" {
-  description = "List of public subnet IDs"
-  value       = aws_subnet.public.*.id
+  description = "List of public subnet IDs for the EKS VPC"
+  value       = [for s in aws_subnet.public : s.id]
+}
+
+output "db_private_subnet_ids" {
+  description = "List of private subnet IDs for the DB VPC"
+  value       = [for s in aws_subnet.db_private : s.id]
 }
